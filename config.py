2f413f72667d2b0f156a138376e5f4d02475c745
Z4rsi0 <89769065+Z4rsi0@users.noreply.github.com>
2021-08-30 13:06:15 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,24 @@
+# ecnitoctoc/config.py
+import tweepy
+import logging
+import os
+
+logger = logging.getLogger()
+
+def create_api():
+    consumer_key = ""
+    consumer_secret = ""
+    access_token = ""
+    access_token_secret = ""
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth, wait_on_rate_limit=True,
+        wait_on_rate_limit_notify=True)
+    try:
+        api.verify_credentials()
+    except Exception as e:
+        logger.error("Error creating API", exc_info=True)
+        raise e
+    logger.info("API created")
+    return api
