6152e60d482b8a540573f8b1f3a9df5124788d6b
Z4rsi0 <89769065+Z4rsi0@users.noreply.github.com>
2021-08-30 13:07:44 +0200

    Create launch.py

---
+++ launch.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import os
+from time import sleep
+
+print("Lancement du bot")
+os.system('echo 1 > /home/twitter/ecnitoctoc/launch.status')
+os.system('python /home/twitter/ecnitoctoc/favretweet.py')   #commande qui lance le programe
+os.system('echo 0 > /home/twitter/ecnitoctoc/launch.status')
+print('Restart en cours')
+sleep(1)
+exit()
